2f3fdc49288adbdb6004f112a23dc7eda00b77db
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-10-25 09:41:41 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log('working')
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let turn ,winner, board
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
